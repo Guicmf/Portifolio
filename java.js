9bae21c4a227c160e831c9efd3259c58f70b7412
Guicmf <gui2007bh@gmail.com>
2024-10-29 12:05:11 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+
+const esquerda = document.querySelector("#left")
+const direita = document.querySelector("#right")
+const about = document.querySelector("#about")
+const habilidades = document.querySelector("#skills")
+
+esquerda.addEventListener("click", ()=>{
+  if(about.classList.contains("active")){
+    habilidades.classList.add("active")
+    about.classList.remove("active")
+    
+  }
+  else {
+    habilidades.classList.remove("active")
+    about.classList.add("active")
+  }
+})
+
+direita.addEventListener("click", ()=>{
+  if(about.classList.contains("active")){
+    habilidades.classList.add("active")
+    about.classList.remove("active")
+    
+  }
+  else {
+    habilidades.classList.remove("active")
+    about.classList.add("active")
+  }
+})
